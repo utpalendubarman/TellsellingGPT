8c7e2ea9216e9a21e9cdcf189f555a0f2c6c0475
s0ubhik <soubhik1809@gmail.com>
2024-01-11 23:03:28 +0530

    added update endpoint

---
+++ projects/app/src/pages/api/admin/update.ts
@@ -0,0 +1,19 @@
+import { spawn } from 'child_process';
+import { promisify } from 'util';
+
+export default async function handler(req, res) {
+    try {
+        const command = "sudo su soubhik -c 'bash $HOME/update > $HOME/update.log &'";
+        const childProcess = spawn(command, {
+            shell: true,
+            detached: true,
+            stdio: 'ignore'
+        });
+        childProcess.unref();
+
+        res.status(200).json({ status: true, message: 'Update Started' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ status: false, message: 'Internal Server Error :(' });
+    }
+}
