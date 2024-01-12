39ae61a7ac3802943112f38bd3d5f67329772eb3
utpalendubarman <47846069+utpalendubarman@users.noreply.github.com>
2024-01-12 15:50:51 +0530

    Update update.ts

--- projects/app/src/pages/api/admin/update.ts
+++ projects/app/src/pages/api/admin/update.ts
@@ -2,18 +2,18 @@ import { spawn } from 'child_process';
 import { promisify } from 'util';
 
 export default async function handler(req, res) {
-    try {
-        const command = "sudo su soubhik -c 'bash $HOME/update > $HOME/update.log 2>&1 &'";
-        const childProcess = spawn(command, {
-            shell: true,
-            detached: true,
-            stdio: 'ignore'
-        });
-        childProcess.unref();
+  try {
+    const command = "sudo su soubhik -c 'bash $HOME/update > $HOME/update.log 2>&1 &'";
+    const childProcess = spawn(command, {
+      shell: true,
+      detached: true,
+      stdio: 'ignore'
+    });
+    childProcess.unref();
 
-        res.status(200).json({ status: true, message: 'OK' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ status: false, message: 'Internal Server Error :(' });
-    }
+    res.status(200).json({ status: true, message: 'OK' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ status: false, message: 'Internal Server Error :(' });
+  }
 }
